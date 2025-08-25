ae91222e6a9fd59bad6ff5e4165ebdc4c8f6d31a
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-08-25 15:36:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-//your JS code here. If required.
 let ageInput=document.getElementById("age");
 let nameInput=document.getElementById("name");
 let submitButton=document.getElementById("btn");
 
-submitButton.addEventListener("click",()=>{
+submitButton.addEventListener("click",(event)=>{
+    event.preventDefault()
     if(ageInput.value=="" || nameInput.value==""){
         alert("Please enter valid details")
     }
     else{
         let promise=new Promise((resolve,reject)=>{
             setTimeout(()=>{
-                if(ageInput.value>18){
-                    resolve("Welcome,. You can vote")
+                if(Number(ageInput.value)>18){
+                    resolve(`Welcome, ${nameInput.value}. you can vote.`)
                 }
                 else{
-                    reject("Oh sorry. You aren't old enough")
+                    reject(`Oh sorry ${nameInput.value}. You aren't old enough`)
                 }
             },4000)
         })
